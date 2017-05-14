ad56635eeba25b5ffda1541633cd102b5db46757
Aryan <aryan.gomes@gmail.com>
2017-05-14 20:58:35 -0300

    creating the migrations and tables

---
+++ database/migrations/2017_05_14_234837_create_table_list_has_items.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableListHasItems extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('list_has_items', function (Blueprint $table) {
+            $table->integer('lists_idList')->unsigned();
+            $table->integer('items_idItem')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('lists_idList')
+                ->references('idList')
+                ->on('lists')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->foreign('items_idItem')
+                ->references('idItem')
+                ->on('items')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('list_has_items');
+    }
+}
