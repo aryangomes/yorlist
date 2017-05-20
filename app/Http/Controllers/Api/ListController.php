1dd26d4c15988f2232cd71904bfad888a9f36f62
Aryan <aryan.gomes@gmail.com>
2017-05-20 20:02:55 -0300

    Auto stash for revert of "update"

---
+++ app/Http/Controllers/Api/ListController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Models\ListModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ListController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $lists = ListModel::where('user_id', app('Dingo\Api\Auth\Auth')->user()->id)->get()->all();
+
+        return response()->json($lists);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $list = new ListModel();
+        $list->fill($request->all());
+        $list->save();
+
+        return response()->json($list, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $list = ListModel::where('idList', $id)->first();
+
+        if (!isset($list)) {
+            return response()->json([
+                'message' => 'Record not found',
+            ], 404);
+        }
+
+        return response()->json($list);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
